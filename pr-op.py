9e2f8a71e8fc7b7203f68ebd0e7b0a377f437df4
pujitha <pujithakankanala@gmail.com>
2025-01-19 19:55:22 -0600

    adding seperator

--- pr-op.py
+++ pr-op.py
@@ -1,4 +1,4 @@
 name=input()
 age=input()
 age=int(age)
-print ("the name of the person is", name, "and the age is ", age)
+print ("the name of the person is ", name, " and the age is ",age,'.',sep="")
