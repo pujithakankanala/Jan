2d2b5ce056701701fcdc72c3b0134e768ca1b747
pujitha <pujithakankanala@gmail.com>
2025-01-21 13:49:50 -0600

    if inside if condition

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,11 @@
 a=int(input())
-if a==4:
-    print(hello)
-elif a>5:
+if a>4:
+    print("hello")
+    if a>10:
+        print("a is greater than 10")
+    else:
+        print("a is ", a)
+elif a<=5:
     print("a is greater than 5")
 else:
-    print("a is not equal 4")
+    print("a is equal 4")
