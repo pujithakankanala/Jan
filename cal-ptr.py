515a6fd739cf3ab7aca5e4800bc37b4a0403aff9
pujitha <pujithakankanala@gmail.com>
2025-01-21 13:43:54 -0600

    if else condition

---
+++ cal-ptr.py
@@ -0,0 +1,5 @@
+p=int(input())
+r=float(input())
+t=int(input())
+si=(p*t*r)/100
+print(int(si))
---
+++ conditionals.py
@@ -0,0 +1,7 @@
+a=int(input())
+if a==4:
+    print(hello)
+elif a>5:
+    print("a is greater than 5")
+else:
+    print("a is not equal 4")
