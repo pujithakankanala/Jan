ad4c2ea19451147f0929e436089289657850e445
pujitha <pujithakankanala@gmail.com>
2025-01-21 14:17:35 -0600

    adding for loop

--- conditionals.py
+++ conditionals.py
@@ -1,4 +1,4 @@
-a=int(input())
+"""a=int(input())
 if a>4:
     print("hello")
     if a>10:
@@ -9,3 +9,14 @@ elif a<=5:
     print("a is greater than 5")
 else:
     print("a is equal 4")
+
+#split funtion
+a,b=input().split()
+a,b=b,a
+#swapping of two numbers
+print(a,b)"""
+
+#for loop
+x=0
+for x in range(0,5,2):
+    print(x)
